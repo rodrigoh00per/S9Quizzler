e62df36bf6ee549122e7f80d68cfefe336021943
rodrigoh00per <rodrigoh00per1@gmail.com>
2019-02-06 02:04:50 -0600

    Proyecto Terminado

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     let allQuestions = QuestionBlank();
     var pickedAnswer:Bool =  false;
     var questionNumber = 0;
+    var score:Int = 0 ;
     var  lengtharray = 0;
 
     //Place your instance variables here
@@ -25,16 +26,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let firstAnswer = allQuestions.list[0];
-        questionLabel.text  = firstAnswer.questionText;
+        nextQuestion();
          lengtharray = allQuestions.list.count;
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
 
-        
-      
         
         if (sender.tag == 1 ){
             pickedAnswer = true;
@@ -46,44 +44,64 @@ class ViewController: UIViewController {
         checkAnswer();
         
         questionNumber = questionNumber + 1;
-   
-        if(questionNumber>allQuestions.list.count - 1 ){
-            questionNumber = 0;
-        }
         
-        
-        questionLabel.text = allQuestions.list[questionNumber].questionText;
-        
-       
-       
-        
+        nextQuestion();
         
     }
     
     
     func updateUI() {
-      
+          scoreLabel.text = "Score: \(score)";
+        progressLabel.text = "\(questionNumber + 1)/\(13)";
+        progressBar.frame.size.width = (view.frame.size.width / 13) *  CGFloat(questionNumber + 1);
     }
     
 
     func nextQuestion() {
+        if(questionNumber < allQuestions.list.count){
+         questionLabel.text = allQuestions.list[questionNumber].questionText;
+            updateUI();
+        }
+        
+        else{
+            //CREATE A SIMPLE ALERT
+            let alert = UIAlertController(title: "Awesome", message: "Do you want to restart the game?", preferredStyle: .alert);
+            
+            //ACTION FOR THE ALERT
+            let restartAction = UIAlertAction(title: "Restart",style:.default,handler: { (UIAlertAction) in
+                self.startOver();
+            });
+        
+            //PUT THE RESET ACTION INTO THE ALERT
+            alert.addAction(restartAction);
+            
+            
+            //SHOW THE ALERT TO THE USER.
+            present(alert,animated: true,completion: nil);
+        }
+        
         
     }
     
     
     func checkAnswer() {
         let correctAnswer = allQuestions.list[questionNumber].answer;
+        
         if(correctAnswer == pickedAnswer){
-            print("Tu respuesta es correcta");
+            score += 1;
+            ProgressHUD.showSuccess("Correct");
         }
         else{
-            print("Tu respuesta es incorrecta");
+            ProgressHUD.showError("Wrong");
         }
     }
     
     
     func startOver() {
-       
+        questionNumber = 0;
+        score = 0 ;
+        nextQuestion();
+ 
     }
     
 
